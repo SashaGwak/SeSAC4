a09cbe2eaaa4961960ef693bcdf367919439b3e8
SihaGwak <sasha.gwak@gmail.com>
2022-07-18 18:25:42 +0900

    study:promise practice

--- nodejs/220718/promise_practics.js
+++ nodejs/220718/promise_practics.js
@@ -1,4 +1,4 @@
-function call(name, cb) {
+function call(name) {
     return new Promise( function (resolve, reject) {
         setTimeout(function() {
             console.log(name);
@@ -24,13 +24,15 @@ function hell(message) {
 call('kim')
     .then( function(name) {
         console.log(name + '반가워');
-        return back(name);
+        var j = back(name);
+        return j
         // return 이 되면 바로 체이닝 된 다음 함수가 진행되는 것
         // 따라서 꼭 return back()이 아니어도 되고, 값만 지정해준다면 return j 이런식으로도 넘어가는 것 가능
     })
     .then( function(txt) {
         console.log(txt + '을 실행했구나');
         return hell(txt);
+        // 이렇게 쓰는 것이 더 간단
     })
     .then( function(message) {
         console.log('여기는' + message);
