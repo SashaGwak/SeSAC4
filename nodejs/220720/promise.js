b279f62a37831f978e4696053cd542c9549751ce
SihaGwak <sasha.gwak@gmail.com>
2022-07-20 14:22:20 +0900

    study: callback, class, promise

---
+++ nodejs/220720/promise.js
@@ -0,0 +1,31 @@
+function promise1(flag){
+    return new Promise( function(resolve, reject) {
+        if(flag) {
+            resolve('then으로 연결');
+        } else {
+            reject('catch로 연결');
+        }
+    });
+}
+
+promise1(true)
+    .then( function(result){
+        console.log('then : ',result);
+    })
+    .catch( function (err) {
+        console.log('catch : ', err);
+    });
+
+promise1(false)
+    .then( function(result){
+        console.log('then : ',result);
+    })
+    .catch( function (err) {
+        console.log('catch : ', err);
+    });
+
+// promise의 상태 
+// 1.pending : promise를 수행중인 상태 
+// 2.Fulfilled : promise가 resolve 된 상태 
+// 3.Rejected : promise가 지켜지지 못한 상태, reject 된 상태
+// 4.Settled : fulfilled 혹은 rejected로 결론이 난 상태
