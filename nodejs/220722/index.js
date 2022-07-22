b265ad5e6264cca6bf7e074a653a8d31a8042844
SihaGwak <sasha.gwak@gmail.com>
2022-07-22 15:18:34 +0900

    study: express module, static

--- nodejs/220722/index.js
+++ nodejs/220722/index.js
@@ -2,26 +2,14 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
-// ejs 사용법
-app.set('view engine', 'ejs');
-// view engine 으로 셋팅하면 views 디렉토리 내에 확장자가 ejs인 파일 이용한다는 것
-
-// 미들 웨어 사용법
 // app.use(express.static('public'));
 // 경로를 안쓸 정적파일 관리하는 곳(public 디렉토리)을 지정해줌
 
 app.use('/abc', express.static('public'));
-// 정적 파일을 가상경로를 어떻게 지정할 것인지 정해줌
+// 정적 파일을 path를 어떻게 지정할 것인지
 
-// express 사용법
 app.get('/', (req,res) => {
-    var list = ['apple', 'peach']; // DB에서 정보를 가져온다면
-    res.render('test', {list: list});
-
-    // render 사용할 때는 확장자 안적어도 된다
-    // res.render("test", {a: 'aaa', b:'bbb'});
-    // back 정보 받는 법
-    // 1. 딕셔너리로 보낸다! -> front에서 변수로 사용할 수 있음
+    res.send('Hello Express!');
 })
 
 app.listen(port, () => {
--- nodejs/220722/npm.md
+++ nodejs/220722/npm.md
@@ -17,3 +17,21 @@ Node Package Manager
     * 설치된 패키지
 
 ## package.json
+
+## Express 
+웹 서버를 생성하는 것과 관련된 기능을 담당하는 프레임워크 
+웹 애플리케이션을 만들기 위한 각종 메소드와 
+* express ()
+    - Express 모듈이 export하는 최상위 함수로, express application을 만듦
+* app 객체
+* app 객체의 method
+    - get : 요청을 받을 때 하는거, 내가 어떤 정보를 원할때, 서버 요청할때 ex)주소에 보임
+    - post : 일반적으로는 뭔가를 생성할때 쓴다. ex)회원가입할 때 새로운 것이 생성되지만 주소에 정보가 보이지 않음
+    - put : 어떤 것을 수정할때 쓴다.(전체) 
+    - delete : 보내는 객체를 삭제 
+    - patch : 수정할 때 쓴다. put은 모두 덮어쓰며 수정되는 반면 patch는 내가 수정할 값(일부)만 바꿀 수 있음 
+
+## 미들웨어
+### static
+* 이미지, css 파일 및 javascript 파일과 같은 정적 파일 제공
+* Express에 있는 static 메소드를 이용해 미들웨어로 로드
