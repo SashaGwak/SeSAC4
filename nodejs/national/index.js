0054c05ccdb964d034da002a97a0e962050f06ac
SihaGwak <sasha.gwak@gmail.com>
2022-07-22 16:42:37 +0900

    study: national project file

---
+++ nodejs/national/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/', (req,res) => {
+    // render 사용할 때는 확장자 안적어도 된다
+    res.render("national");
+})
+
+app.listen(port, () => {
+    console.log( 'Server port : ', port );
+})
