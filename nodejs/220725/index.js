393cd29852dfa4298a39709c313a723db10cce43
SihaGwak <sasha.gwak@gmail.com>
2022-07-25 19:01:19 +0900

    stduy: 실습 회원가입 만들고 info.txt 파일 연결하기

--- nodejs/220725/index.js
+++ nodejs/220725/index.js
@@ -2,27 +2,22 @@ const express = require('express');
 const app = express();
 const port = 8080;
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 app.set('view engine', 'ejs');
 app.use( express.static('public'));
-app.use( express.urlencoded({extended: true}));
+app.use( express.urlencoded({extended: false}));
+// 데이터 인코딩 방식 설정, false를 하면 object 뎁스가 있는 값일 경우 뎁스를 잘 나누어서 값을 보내준다
 app.use( bodyParser.json());
 // express 서버가 데이터를 받을 때 json 형태로 받겠다고 미리 선언한 것
 
 app.get('/', function(req,res) {
-    res.render('practice_index');
+    res.render('practice2_index');
 });
 
-app.get('/receive', function(req,res) {
-    console.log( req.body )
-    console.log( req.query )
-    // post는 body지만 get으로 하면 url 주소 내 query로 받기때문에 주의 !
-    res.render('practice_receive', req.query);
-});
-
-app.post('/receive', function(req,res) {
-    console.log( req.body )
-    res.render('practice_receive', req.body);
+app.post('/login', function(req,res) {
+    res.render('practice2_receive');
+    fs.writeFile('./info.txt', req.body.name+'//'+req.body.id+'//'+req.body.password);
 });
 
 app.listen(port, () => {
