8cf737161a8931343ecacc1d59badd0f6fb91410
sihagwak <sasha.gwak@gmail.com>
2022-07-26 18:31:04 +0900

    study: login system practice

---
+++ nodejs/login/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+app.set('view engine', 'ejs');
+app.use( express.static('public'));
+app.use( express.urlencoded({extended: true}));
+app.use( bodyParser.json());
+
+app.get('/', function(req,res) {
+    res.render("index");
+});
+
+app.post('/', function(req,res) {
+    console.log(req.body);
+    fs.readFile('./info.txt', 'utf8', (err, data) => {
+        if (err) {
+            console.log('fs error');
+        } else {
+            var info = data.split('//');
+            if ((req.body.id === info[0]) && (req.body.pw === info[2])) {
+                res.send("<script>alert('로그인 성공!');</script>");
+            } else {
+                res.send("<script>alert('아이디와 비밀번호를 확인해주세요!');</script>");
+            }
+        }
+    })
+});
+
+app.listen(port, function() {
+    console.log('Server port : ', port); 
+});
