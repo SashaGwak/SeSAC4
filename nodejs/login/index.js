0d532d1e82fe18da4230052313b563bd1bf615f7
SihaGwak <sasha.gwak@gmail.com>
2022-07-27 16:57:50 +0900

    study: login upgrade with Axios (빈공간에 정보띄우기)

--- nodejs/login/index.js
+++ nodejs/login/index.js
@@ -13,18 +13,22 @@ app.get('/', function(req,res) {
     res.render("index");
 });
 
-app.post('/', function(req,res) {
+app.post('/login', function(req,res) {
     console.log(req.body);
     fs.readFile('./info.txt', 'utf8', (err, data) => {
         if (err) {
             console.log('fs error');
         } else {
             var info = data.split('//');
-            if ((req.body.id === info[0]) && (req.body.pw === info[2])) {
-                res.send("<script>alert('로그인 성공!');</script>");
-            } else {
-                res.send("<script>alert('아이디와 비밀번호를 확인해주세요!');</script>");
-            }
+            var id = info[0];
+            var pw = info[2];
+            res.send({id:id, pw:pw})
+            // axios 안쓰면 원래 이렇게 해줬음!!
+            // if ((req.body.id === info[0]) && (req.body.pw === info[2])) {
+            //     res.send("<script>alert('로그인 성공!');</script>");
+            // } else {
+            //     res.send("<script>alert('아이디와 비밀번호를 확인해주세요!');</script>");
+            // }
         }
     })
 });
