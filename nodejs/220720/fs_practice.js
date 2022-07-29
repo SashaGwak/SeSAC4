c078269ee957ae2f667f266c74fcc6b885ee0d3f
SihaGwak <sasha.gwak@gmail.com>
2022-07-29 18:10:28 +0900

    study: fs 비동기 방식 실행

--- nodejs/220720/fs_practice.js
+++ nodejs/220720/fs_practice.js
@@ -1,6 +1,8 @@
 const fs = require('fs').promises;
 
 async function exec(){
+    // async await 써서 파일 만들어지는 것 읽는 것 등 기다려줘야 온전한 파일을 다룰 수 있음 !! 
+    // 비동기 방식을 안쓰면 파일 내용이 다 안불러와졌는데 copy하거나 이런경우가 생겨요 ! 
     await fs.mkdir('./sesac');
     await fs.writeFile('./sesac/sesac.txt', '반갑습니다');
     await fs.copyFile('./sesac/sesac.txt', './sesac/sesac2.txt');
