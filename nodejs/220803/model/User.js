5085873bd15e60ccb01cbb9f37937c7603ff174a
SihaGwak <sasha.gwak@gmail.com>
2022-08-03 18:16:07 +0900

    study: MVC모델로 회원가입 및 로그인하기, 여러 유저 가능하도록 구현

--- nodejs/220803/model/User.js
+++ nodejs/220803/model/User.js
@@ -1,20 +1,34 @@
 const fs = require('fs').promises;
 
+
 exports.post_user = function(data){
+    console.log(data);
     const {id, pw, name, age} = data; 
-    fs.writerFile('./info.txt', '${id}//${pw}//${name}//${age}');
-    // console.log(data);
-    // console.log(data.name);
-    // let name = data.name; 
-    // let msg = data.name + '님 회원가입이 완료되었습니다!🥰';
-    // console.log(msg);
-    // return name, msg 
+    console.log(id);
+    fs.appendFile('./info.txt', id + '//' + pw + '//' + name + '//' + age + '\n', function(err) {
+        if (err) throw err;
+        console.log('회원가입 정보 추가 오류')
+    })
 }
+
+/*
+model과 controller 정보 주고받는 방법 !! 
+exports.post_user = function(data){
+    console.log(data);
+    let name = data.name; 
+    let msg = data.name + '님 회원가입이 완료되었습니다!🥰';
+    console.log(name);
+    console.log(msg);
+    return msg;
+}
+*/
+
 exports.get_user = async function() {
     var buffer = await fs.readFile('./info.txt');
     return buffer.toString();
 }
 /*
+파일 읽어오는 법 
 exports.post_user = function(data){
     const {id, pw, name, age} = data; 
     fs.writerFile('./info.txt', '${id}//${pw}//${name}//${age}')
