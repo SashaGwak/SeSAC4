c1770dc291ebbd346478ad2b0d553a2adc91da6a
SihaGwak <sasha.gwak@gmail.com>
2022-08-03 21:12:27 +0900

    study: 회원가입파일에 파일업로드 추가, 파일 저장까지는 진행, 이후 view연결 필요

--- nodejs/220803/routes/index.js
+++ nodejs/220803/routes/index.js
@@ -4,16 +4,26 @@ const userRouter = express.Router();
 // userRouter가 Router라고 지정해줌
 const user = require('../controller/UserController');
 // 각각의 객체 가져와도 되지만 user.객체명으로 써서 여러 객체 사용할 수 있게 하기 위해
+const multer = require('multer');
+const path = require('path');
+const storage = multer.diskStorage({
+    destination(req, file, cb){
+        cb(null, 'public/')
+    },
+    filename(req, file, cb){
+        const ext = path.extname(file.originalname);
+        cb(null, req.body.id + ext);
+    }
+}); 
+const upload = multer({storage});
 
 userRouter.get('/', user.index); 
 userRouter.get('/register', user.register);
-userRouter.post('/register', user.post_register);
+userRouter.post('/register', upload.single('userfile'), user.post_register);
 // UserController파일에서 export한 index, register 객체 가져온 것
 
 userRouter.get('/login', user.login);
 userRouter.post('/login', user.post_login);
-
-userRouter.get('/profile', upload.single('userfile'), user.profile);
 
 module.exports = userRouter; 
 // index.js파일에서 router를 모르기때문에 내보내서 알려줘야함 
