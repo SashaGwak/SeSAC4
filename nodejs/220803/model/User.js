adc454187449824b87e472d7a2c2169848b79c12
sihagwak <sasha.gwak@gmail.com>
2022-08-05 01:58:42 +0900

    study: 회원가입 -> 로그인 -> 프로파일 순으로 연동하기

--- nodejs/220803/model/User.js
+++ nodejs/220803/model/User.js
@@ -1,14 +1,17 @@
 const fs = require('fs').promises;
 
-
+// 로그인 정보와 파일정보 받아오기 
 exports.post_user = function(data, file){
     console.log(data);
     console.log(file);
-    const {id, pw, name, age} = data; 
+    const {id, pw, name, age} = data;
+    // req.body 부분 나눠주기  
     const {filename} = file; 
+    // req.file에서 filename 추출하기 
     fs.appendFile('./info.txt', id + '//' + pw + '//' + name + '//' + age + '//'+ filename + '\n', function(err) {
+        // appendFile -> 이어서 써주기 
         if (err) throw err;
-        console.log('회원가입 정보 추가 오류')
+        console.log('회원가입 정보 추가 오류');
     })
 }
 
@@ -26,7 +29,9 @@ exports.post_user = function(data){
 
 exports.get_user = async function() {
     var buffer = await fs.readFile('./info.txt');
+    // 읽는 거 기다려주도록 async await 써주기 
     return buffer.toString();
+    // 읽을 수 있도록 toString 으로 함수 넣어주기 
 }
 /*
 파일 읽어오는 법 
