7eac7611edae7235306a081099f9e611e98f814c
sihagwak <sasha.gwak@gmail.com>
2022-08-05 01:58:42 +0900

    study: 회원가입 -> 로그인 -> 프로파일 순으로 연동하기

--- nodejs/220803/controller/UserController.js
+++ nodejs/220803/controller/UserController.js
@@ -11,6 +11,7 @@ exports.register = (req, res) => {
 
 exports.post_register = async (req, res) => {
     var data = await User.post_user(req.body, req.file);
+    // data 가공하는 거 기다려주기 
     console.log(req.file);
     res.send('회원가입이 완료되었습니다!')
 }
@@ -22,9 +23,13 @@ exports.post_register = (req, res) => {
     res.send(User.post_user(req.body));
 }
 */
+
+// 처음 로그인 페이지에서 login.ejs 렌더링하기 
 exports.login = (req, res) => {
     res.render('login'); 
 }
+
+// 아이디 비밀번호 확인 시스템 및 profile로 정보전달
 exports.post_login = async (req, res) => {
     var data = await User.get_user();
     
@@ -38,6 +43,7 @@ exports.post_login = async (req, res) => {
         if (user[0] == req.body.id && user[1] == req.body.pw){
             console.log(user);
             res.render('profile', {id: user[0], pw: user[1], name:user[2], age:user[3], filename:user[4]});
+            // profile 로 정보전달 -> 리스트 -> 딕셔너리 형태로 만들어서 정보 내보내줌! 
             break;
         };
         if (i == info.length -1) {
