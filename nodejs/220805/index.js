7fea4545b64d4b21dc763a56ea140c6b12be8420
SihaGwak <sasha.gwak@gmail.com>
2022-08-05 13:43:00 +0900

    study: 220805 방명록 view 만들기 완료

---
+++ nodejs/220805/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+const path = require('path');
+
+// serve static files
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+const bodyParser = require('body-parser');
+app.use( express.urlencoded({extended: true}));
+app.use( bodyParser.json());
+
+const router  = require('./routes');
+app.use('/', router);
+
+app.listen(port, () => {
+    console.log('Server Port : ', port);
+});
