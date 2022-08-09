abf197259f0cb0ba8a89bf5b245f2f93506b32d9
sihagwak <sasha.gwak@gmail.com>
2022-08-09 23:29:43 +0900

    실습 login database 연결해서 CRUD 구현하기(모델 보기편하게수정)

--- nodejs/220808/controller/Controller.js
+++ nodejs/220808/controller/Controller.js
@@ -1,5 +1,5 @@
 const User = require('../model/User');
-
+// model의 정보 불러오기
 
 exports.register = (req, res) => {
     res.render('register'); 
@@ -8,6 +8,8 @@ exports.post_register = (req, res) => {
     console.log(req.body);
 
     User.insert(req.body.id, req.body.name, req.body.pw, req.body.email, function (result){
+        // function(result) 가 model/User.js의 User.insert(cb)의 콜백함수가 된 것! 
+        // 따라서 콜백함수를 통해 rows=result 정보를 전달받을 수 있다
         console.log(result);
         res.send('회원가입 성공!'); 
     })
@@ -20,17 +22,12 @@ exports.login = (req, res) => {
 exports.post_login = (req, res) => {
     User.login(req.body.id, req.body.pw, function(result) {
         console.log('요긴 컨트롤 : ', result);
+        // 로그인 위하여 id, pw 확인 작업
         if (req.body.id == result.id && req.body.pw == result.password) {
             res.render('profile', {id: req.body.id, pw: req.body.pw}); 
             return; 
         }
         res.render('fail'); 
-        // if (req.body.id == result.id && req.body.pw == result.password) {
-        //     res.render('profile');
-        //     return;
-        // }else {
-        // res.send(msg);     
-        // }
     }); 
     }
 
--- nodejs/220808/model/User.js
+++ nodejs/220808/model/User.js
@@ -1,15 +1,19 @@
 const mysql = require('mysql'); 
 
+// createConnection : DB 연결해주는 함수 
 const cnn = mysql.createConnection({
     host:'localhost',
     user: 'user',
     password: 'Mini1028!', 
     database: 'sesac',
+    // 하나의 DB만 연결가능하므로 DB명 명시해줘야함 
 })
 
-
+// register와 연결되어 회원가입 -> insert가능하게 해줌
 exports.insert = (id, name, pw, email, cb) => {
+    // DB.query -> SQL문으로 DB 다뤄줌 
     var sql = `insert into user(id, name, password, email) values('${id}', '${name}', '${pw}', '${email}')`
+    // 백틱사용하고 있음!! 
     cnn.query(sql, (err, rows) => {
         if (err) throw err;
 
@@ -18,16 +22,22 @@ exports.insert = (id, name, pw, email, cb) => {
     })
 }
 
+// 로그인과 연결되어 id(primary key) 정보를 통해 user 정보확인해줌
 exports.login = (id, pw, cb) => {
     var sql = `select * from user where id='${id}'`
     cnn.query(sql, (err,rows) => {
         if (err) throw err; 
 
         console.log('DB : rows', rows);
+        // 정상적으로 실행되면 rows에 데이터 모두 담긴다
+        // 하지만 [{data}] 형태임에 주의하기!!! 
+        // 여기서는 한 user의 데이터만 써줄거니까 rows[0]로 기재 
+        // -> {data} 형태로 표현됨
         cb(rows[0]); 
     }); 
 }
 
+// profile 과 연결되어 user정보 update 구현
 exports.update = (data, cb) => {
     let sql = `update user set password='${data.pw}' where id='${data.id}';`
     cnn.query(sql, (err, rows) => {
@@ -37,6 +47,7 @@ exports.update = (data, cb) => {
     })
 }
 
+// profile과 연결되어 회원 탈퇴 가능하도록 구현
 exports.delete = (data, cb) => {
     cnn.query(`delete from user where id='${data.id}';`, (err, rows) => {
         if (err) throw err; 
