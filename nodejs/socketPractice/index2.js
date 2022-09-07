dee9120d963c11e7e96b08c8475e75ad622a8f5f
SihaGwak <sasha.gwak@gmail.com>
2022-09-07 17:42:25 +0900

    study: socket practice 입장, 퇴장, 보내는 사람 받는 사람 구분하기

--- nodejs/socketPractice/index2.js
+++ nodejs/socketPractice/index2.js
@@ -8,23 +8,24 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index2.html');
 })
 
+// io -> 클라이언트와의 모든 연결을 갖고있는 친구 
+// socket -> 클라이언트 한명. socket.id가 클라이언트를 구분하는 식별자
 io.on('connection', function (socket) {
-    console.log('connected')
-    socket.emit('hello', 'server hello')
+    console.log('connected', socket.id);
+    socket.emit('info', socket.id);
+    // 아이디 값 보내줌
+    io.emit('notice', socket.id + '님이 입장하셨습니다.');
+    socket.on('send', function(data) {
+        // data = {id: ~~~. msg: ~~~~ };
+        console.log('client message : ', data.msg); 
+        io.emit('newMessage', data);
+    })
 
-    socket.on('hello', function(data){
-        console.log('click :', data); 
-        socket.emit('helloResponse', '안녕'); 
-    })
-    socket.on('study', function(data){
-        console.log('click :', data); 
-        socket.emit('studyResponse', '공부하자!'); 
-    })
-    socket.on('bye', function(data){
-        console.log('click :', data); 
-        socket.emit('byeResponse', '바이'); 
+    socket.on('disconnect', function(){
+        io.emit('notice', socket.id +'님이 퇴장하셨습니다.');
     })
 });
+
 
 http.listen(8000, function(){
     console.log( "Server Port : ", 8000 );
