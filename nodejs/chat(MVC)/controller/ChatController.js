0d388a52674d9e0c6b3d6b6c1106c00968595c7e
SihaGwak <sasha.gwak@gmail.com>
2022-09-07 19:37:23 +0900

    study: 나만의 채팀 시스템 만들어보기

---
+++ nodejs/chat(MVC)/controller/ChatController.js
@@ -0,0 +1,8 @@
+exports.chat_index = (req, res) => {
+    // 인덱스 화면 보여주기 
+    res.render('index');
+};
+
+exposts.post_name = (req, res) => {
+    
+}
--- nodejs/chat/controller/ChatController.js
+++
@@ -1,4 +0,0 @@
-exports.chat_index = (req, res) => {
-    // 닉네임 정하는 화면 보여주기 
-    res.render('index');
-}
