a44e851baf9e9fd175331053c1ec53fe712405c4
sihagwak <sasha.gwak@gmail.com>
2022-09-22 23:37:14 +0900

    study: node.js 기본개념 이해

--- nodejs/220918/app.js
+++ nodejs/220918/app.js
@@ -2,7 +2,7 @@ const http = require('http');
 // /로 시작하면 절대경로 ./로시작하면 상대경로
 // 경로를 생략하면 http라는 글로벌 모듈을 찾게 되는 것 
 const port = 3000;
-const fs = require('fs');
+const routes = require('./routes')
 
 /*
 createServer(requestListener (request, response) => void) : Sever
@@ -20,61 +20,10 @@ requestListener -> 들어오는 모든 요청을 실행하는 기능
 // });
 
 // 방법 3 (화살표 함수 이용)
-const server = http.createServer((req, res) => {
-    console.log(req.url , req.method, req.headers);
-    // process.exit();
-    // 프로세스 중지(대게는 서버 중지하지 않기때문에 쓸일 거의 없음)
-    // 이벤트 루트를 종료시켜 버림 
+// createServer((req, res) => {
+//})
 
-    const url = req.url; 
-    const method = req.method; 
-    if (url == '/') {
-        res.write('<html>');
-        res.write('<head><title>Enter Message</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text"><button>Send</button></form></body>');
-        res.write('</html>');
-        return res.end();
-        // 이후 다른 리스폰스 호출되서는 안되기 때문 
-    }
-    if (url === '/message' && method === 'POST') {
-        const body = [];
-        // data 이벤트 리스너 생성
-        req.on('data', (chunk) => {
-            console.log(chunk);
-            body.push(chunk);    
-        });
-
-        req.on('end', () => {
-            // 전역에서 사용가능한 Buffer 객체 사용하여 새 buffer 생성
-            const parseBody = Buffer.concat(body).toString();
-            // 버퍼 안에 위에서 모은 모든 청크 추가해줌(어차피 파일이나 이런거 안들어오고 문자열 입력해줄거니까 toString으로 바꿔줌~~)
-            console.log(parseBody);
-        });
-
-        fs.writeFileSync('message.txt', 'DUMMY');
-        // res.writeHead();
-        // writeHead : 한번의 몇가지 메타 정보를 작성할 수 있게하며 다음으로 경로 재지정을 의미하는 상태코드 302를 설정. 이후 JS 객체와 우리가 설정하길 원하는 헤더를 전달
-
-        // 다른 방법
-        res.statusCode = 302; 
-        res.setHeader('Location', '/'); 
-        // 위치 다시 /로 옮겨주면서 Status Code 던져줌 
-        return res.end();
-
-    }
-    res.setHeader('Content-Type', 'text/html'); 
-    // setHeader : 새로운 헤더를 설정할 수 있음
-    // Content-Type: 브라우저가 알고 이해하며 받아들이는 디폴드 헤더 
-    // 두번째인자에 대응될 콘텐츠 유형 적어줌 -> 여기서는 응답의 일부가 될 콘텐츠 유형은 HTML이라는 정보 전달  
-
-    res.write('<html>');
-    // write는 response에 데이터를 기록할 수 있게 함
-    res.write('<head><title>My First Page</title></head>');
-    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-    res.write('</html>');
-
-    res.end(); // 응답을 끝내는 것을 알려줌. 이후에는 당근 respons주면 안됨!
-})
+const server = http.createServer(routes);
 
 server.listen(port, () => {
     console.log('Server port : ', port);
