83d161be3165c2d1bd6e9590ba1d7f3a3d90d80d
SihaGwak <sasha.gwak@gmail.com>
2022-09-23 17:49:19 +0900

    study: event handling

--- React/220923/src/index.js
+++ React/220923/src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Classpractice from './Classpractice';
+import FunctionPractice from './FunctionPractice';
 import StateClass from './StateClass';
 import StateFunction from './StateFunction';
+import Event from './Event';
+import EventClass from './EventClass';
+import ManyInput from './ManyInput';
+import EventEdu from './EventEdu';
+import EventEdu2 from './EventEdu2';
+import Handler_ex from './Handler_ex';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
+    <Handler_ex />
+    <EventEdu2 />
+    <EventEdu />
+    <ManyInput />
+    <EventClass />
+    <Event />
+    <FunctionPractice />
+    <Classpractice />
     <StateClass />
     <StateFunction />
   </>
