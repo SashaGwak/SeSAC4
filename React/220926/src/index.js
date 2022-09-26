87650c28c9112b1d9d0640c036a21e4b5173c83a
SihaGwak <sasha.gwak@gmail.com>
2022-09-26 18:03:49 +0900

    study: practice 59

--- React/220926/src/index.js
+++ React/220926/src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Borad from './Board';
+import Event_ex from './ex/Event_ex';
+import Practice59 from './Practice59';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
     <App />
+    <Event_ex />
+    <Borad />
+    <Practice59 />
   </>
 );
