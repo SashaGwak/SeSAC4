6660b6d0b131b6b4fc025978973573b05e6b7650
SihaGwak <sasha.gwak@gmail.com>
2022-09-28 15:50:52 +0900

    study: scroll practice

--- React/220928/src/index.js
+++ React/220928/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import Ref from './Ref';
+import ScrollBox from './ScrollBox';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ScrollBox />
     <App />
     <Ref />
     <Ref />
