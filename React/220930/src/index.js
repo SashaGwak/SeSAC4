720e5984cbfb469d356ef9b77172f257328b5362
SihaGwak <sasha.gwak@gmail.com>
2022-09-30 16:33:08 +0900

    study: many css library

--- React/220930/src/index.js
+++ React/220930/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import Effect from './1HooksEffect';
 import Reducer from './2HooksReducer';
 import Memo from './3HookMemo';
+import Origin from './4Origin';
+import UseSass from './5UseSacc';
+import CSSModule from './6CSSModule';
+import StyledComponent from './7StyledComponent';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,5 +14,11 @@ root.render(
     <Effect />
     <Reducer />
     <Memo />
+    <Origin />
+    <hr />
+    <UseSass />
+    <hr />
+    <CSSModule />
+    <StyledComponent />
   </>
 );
