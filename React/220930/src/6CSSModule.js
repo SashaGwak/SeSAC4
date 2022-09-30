66829e1456ef40b29f715138a9e7b7a714ea4173
SihaGwak <sasha.gwak@gmail.com>
2022-09-30 17:43:24 +0900

    study: css practice

--- React/220930/src/6CSSModule.js
+++ React/220930/src/6CSSModule.js
@@ -1,8 +1,8 @@
-import style from './6CSSModule.css';
+import style from './6CSSModule.module.css';
 
 const CSSModule = () => {
     return (
-        <div className={style.box1}>
+        <div className={`${style.box1} ${style.center}`}>
         {/* <div className={${style.box1} ${style.center}}> */}
             <h2>Css module</h2>
         </div>
