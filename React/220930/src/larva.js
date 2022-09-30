1ec7426086b61a563b20b9488fbe50d1f1f7d4fd
SihaGwak <sasha.gwak@gmail.com>
2022-09-30 17:43:12 +0900

    study: css practice

---
+++ React/220930/src/larva.js
@@ -0,0 +1,19 @@
+import grass from './grass.png';
+import './larva.scss';
+const Worm = () => {
+    return (
+        <div className="worm">
+            <div className="face">
+                <div className="white_eye"></div>
+                <div className="black_eye"></div>
+            </div>
+            <div className="body1">
+                <div className="body2"></div>
+                <div className="body3"></div>
+                <div className="body4"></div>
+            </div>
+            <img src={grass} />
+        </div>
+    );
+}
+export default Worm;
