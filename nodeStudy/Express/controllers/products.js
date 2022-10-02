846656b575d2a6f7344d99c15d0c9268c3b52749
sihagwak <sasha.gwak@gmail.com>
2022-10-02 21:47:28 +0900

    feat: navbar update, views fileupdate

--- nodeStudy/Express/controllers/products.js
+++ nodeStudy/Express/controllers/products.js
@@ -2,7 +2,7 @@ const Product = require('../models/product');
 
 // 상품 검색 페이지 
 exports.getAddProduct = (req, res, next) => {
-  res.render('add-product', {
+  res.render('admin/add-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
     formsCSS: true,
@@ -21,7 +21,7 @@ exports.postAddProduct = (req, res, next) => {
 // 검색 후 상품 보여주기
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
-    res.render('shop', {
+    res.render('shop/product-list', {
       prods: products,
       pageTitle: 'Shop',
       path: '/',
