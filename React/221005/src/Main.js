2616c3b380e7b6cba0fedf4fbc25f28d26fb6f57
SihaGwak <sasha.gwak@gmail.com>
2022-10-05 15:31:46 +0900

    study: router 실습

--- React/221005/src/Main.js
+++ React/221005/src/Main.js
@@ -1,7 +1,16 @@
+import { Link, useLocation } from "react-router-dom";
+
 function Main() {
+  const location = useLocation();
   return (
-    <div className="Main">
-      <h1>Main</h1>
+    <div>
+      <h1>Main state = {location.state}</h1>
+      
+      <Link to='/product/1'>1번 상품</Link>
+      <br />
+      <Link to='/product/2'>2번 상품</Link>
+      <br />
+      <Link to='/product/2?search=productName&q=demo#test'>2번 상품 상세</Link>
     </div>
   );
 }
