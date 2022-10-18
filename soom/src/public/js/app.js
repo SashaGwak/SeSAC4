33516d90aadfc9615f28fbe981047a202f2d7128
sihagwak <sasha.gwak@gmail.com>
2022-10-19 03:23:10 +0900

    feat: websocket object 보내기, 닉네임 지정하기 기능

--- soom/src/public/js/app.js
+++ soom/src/public/js/app.js
@@ -1,1 +1,44 @@
-alert('hi');
+const messageList = document.querySelector('ul');
+const nickForm = document.querySelector('#nick');
+const messageForm = document.querySelector('#message');
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload) {
+  // Object 만들기 
+  const msg = { type , payload }; 
+  // string으로 바꾸기! (js object를 보내지말고 string으로 보내줄거라서! )
+  // 웹 소켓이 브라우저에 있는 api 이기 때문에 백엔드에서는 다양한 프로그래밍언어를 사용할 수 있기 때문에 api에서 판단을 하면 안됨 백엔드에서 알아서 쓰도록 스트링으로 고고 
+  return JSON.stringify(msg);
+}
+
+socket.addEventListener('open', () => {
+  console.log('Connected to Server ✅');
+});
+
+socket.addEventListener('message', (message) => {
+  const li = document.createElement('li');
+  li.innerText = message.data;
+  messageList.append(li);
+  // console.log('New message : ', message.data);
+});
+
+socket.addEventListener('close', () => {
+  console.log('Disconnected to Server ❌');
+});
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector('input');
+  socket.send(makeMessage('new_message', input.value));
+  input.value = '';
+};
+
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = nickForm.querySelector('input');
+  socket.send(makeMessage('nickname', input.value));
+  input.value = '';
+};
+
+messageForm.addEventListener('submit', handleSubmit);
+nickForm.addEventListener('submit', handleNickSubmit);
--- soom/src/server.js
+++ soom/src/server.js
@@ -1,5 +1,5 @@
 import http from 'http';
-import Websocket, {WebSocketServer} from 'ws';
+import { WebSocketServer } from 'ws';
 import express from 'express';
 
 const app = express();
@@ -17,9 +17,33 @@ const server = http.createServer(app);
 const wss = new WebSocketServer({server});
 // 이렇게 하면 http서버, websocket 서버 둘 다 돌릴 수 있음(꼭 이렇게 안해도 됨)
 
-function handleConnection(socket) {
-    console.log(socket)
-}
+const sockets = [];
+
+wss.on('connection', (socket) => {
+  // 소켓 정보 저장 
+  sockets.push(socket); 
+  socket['nickname'] = 'Anon'; // 익명 값 주기 
+  // 현재 소켓상태를 보여주기 위해
+  // console.log(socket);
+  console.log('Connected to Browser ✅');
+  socket.on('close', () => console.log('Disconnected to the Browser ❌')); 
+  socket.on('message', (msg) => {
+    // { type: 'nickname', payload: 'siha' }
+    const message = JSON.parse(msg.toString()); 
+    // message는 객체 형태, msg는 JSON 형태 
+    console.log(message, msg.toString()); 
+    // 연결된 socket들을 aSocket으로 표시하고 다 보내줌 
+    switch (message.type) {
+      case "new_message": 
+        sockets.forEach((aSocket) => aSocket.send(`${socket.nickname} : ${message.payload}`));
+      case "nickname": 
+        // socket은 기본적으로 객체라서 원하는 내용 추가할 수 있음 
+        socket["nickname"] = message.payload;
+    }
+    // socket.send(message.toString('utf8'));
+    // console.log(message.toString('utf8'));
+  });
+});
 
 server.listen(3000, handleListen);
 
